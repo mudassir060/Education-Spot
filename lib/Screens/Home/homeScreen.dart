c9c02bb1e74eeae79ba4217774267905e88696f0
mudassir060 <mudassirmukhtar4@gmail.com>
2022-09-21 09:11:30 -0700

    Home Screen

--- lib/Screens/Home/homeScreen.dart
+++ lib/Screens/Home/homeScreen.dart
@@ -2,10 +2,8 @@ import 'package:education_spot/Widgets/mySpacer.dart';
 import 'package:education_spot/constants/style.dart';
 import 'package:flutter/material.dart';
 
+import '../../Widgets/ImageButtonGrid.dart';
 import '../../constants/images.dart';
-import '../Jobs/jobsScreen.dart';
-import '../Scholarships/sholarshipsScreen.dart';
-import 'imageButton.dart';
 
 class homeScreen extends StatefulWidget {
   const homeScreen({Key? key}) : super(key: key);
@@ -15,90 +13,66 @@ class homeScreen extends StatefulWidget {
 }
 
 class _homeScreenState extends State<homeScreen> {
+  var buttonList = [
+    {"name": "Scholarships", "image": CommonRoom},
+    {"name": "Admissions", "image": Discussion},
+    {"name": "Quiz", "image": Scholarships2},
+    {"name": "Jobs", "image": Jobs},
+  ];
+
   @override
   Widget build(BuildContext context) {
+    var data = buttonList.length;
+    var quot = (data / 2).floor();
+    var rem = data.remainder(2);
+    if (rem >= 1) {
+      data = quot + 1;
+    } else {
+      data = quot;
+    }
     var vwidth = MediaQuery.of(context).size.width;
     var vheight = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-          body: Column(
+          body: SingleChildScrollView(
+            child: Column(
         children: [
-          mySpacer(10.0, 0.0),
-          // // // // // // // // // // // Top Bar // // // // // // // // //
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              const CircleAvatar(
-                backgroundColor: primaryColor,
-                child: Icon(
-                  Icons.book,
-                  color: Colors.white,
+            mySpacer(10.0, 0.0),
+            // // // // // // // // // // // Top Bar // // // // // // // // //
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                const CircleAvatar(
+                  backgroundColor: primaryColor,
+                  child: Icon(
+                    Icons.book,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              const Text(
-                "Education Spot",
-                style: TextStyle(
-                    color: primaryColor,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25),
-              ),
-              IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.more_vert,
-                    color: primaryColor,
-                  ))
-            ],
-          ),
-          // // // // // // // // // // // Banner Ad // // // // // // // //
-          Image.asset(Banner_Ad ),
-          // // // // // // // // // // // Gridview Button // // // // // // // // //
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              imageButton(
-                img: Scholarships,
-                titel: 'Scholarships',
-                fun: () {
-                  print("Scholarships");
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => sholarshipsScreen()),
-                  );
-                },
-              ),
-              imageButton(
-                img: Admassions,
-                titel: 'Admassion',
-                fun: () {},
-              ),
-            ],
-          ),
+                const Text(
+                  "Education Spot",
+                  style: TextStyle(
+                      color: primaryColor,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25),
+                ),
+                IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.more_vert,
+                      color: primaryColor,
+                    ))
+              ],
+            ),
+            // // // // // // // // // // // Banner Ad // // // // // // // //
+            Image.asset(Banner_Ad ),
+            // // // // // // // // // // // Gridview Button // // // // // // // // //
+            ImageButtonGrid(vheight: data.toDouble(), Community: buttonList,)
 
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              imageButton(
-                img: Quizz,
-                titel: 'Quizz',
-                fun: () {},
-              ),
-              imageButton(
-                img: Available_Jobs,
-                titel: 'JObs',
-                fun: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => jobsScreen()),
-                  );
-                },
-              )
-            ],
-          ),
+
         ],
-      )),
+      ),
+          )),
     );
   }
 }

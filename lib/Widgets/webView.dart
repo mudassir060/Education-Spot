ee1e6019336ea0307b044908bee86f45be865201
mudassir060 <mudassirmukhtar4@gmail.com>
2022-11-25 21:25:25 +0500

    webView

---
+++ lib/Widgets/webView.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class webView extends StatefulWidget {
+  final String url;
+
+  const webView({super.key, required this.url});
+
+  @override
+  webViewState createState() => webViewState();
+}
+
+class webViewState extends State<webView> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable virtual display.
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WebView(
+      initialUrl: widget.url,
+    );
+  }
+}

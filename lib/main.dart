7f8c7e546912c9612930aa1a9b8ecffbf0b34f6e
mudassir060 <mudassirmukhtar4@gmail.com>
2022-12-01 09:27:29 +0500

    firebase installation

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,38 @@
-import 'package:education_spot/Screens/Quizz/QuizzScreen.dart';
-import 'package:education_spot/Screens/authentication/signInScreen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
-import 'Screens/Chats/PriviteChat/inbox_screen.dart';
-import 'Screens/Quizz/QuizQuestion.dart';
-import 'Widgets/BottomNavigBar.dart';
+import 'Screens/authentication/signInScreen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Education Spot',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const signInScreen());
+        home: FutureBuilder(
+          // Initialize FlutterFire
+          future: Firebase.initializeApp(),
+          builder: (context, snapshot) {
+            // Check for errors
+            if (snapshot.hasError) {
+              return const Text("Some thing Went Wrong");
+            }
+
+            // Once complete, show your application
+            if (snapshot.connectionState == ConnectionState.done) {
+              return signInScreen();
+            }
+
+            // Otherwise, show something whilst waiting for initialization to complete
+            return const Center(child: CircularProgressIndicator());
+          },
+        ));
   }
 }

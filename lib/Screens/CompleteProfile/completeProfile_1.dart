122123f6d1cc552bb25f73b99cf1c3798272394d
mudassir060 <mudassirmukhtar4@gmail.com>
2023-03-04 10:43:30 +0500

    pass next screnn data

--- lib/Screens/CompleteProfile/completeProfile_1.dart
+++ lib/Screens/CompleteProfile/completeProfile_1.dart
@@ -41,61 +41,63 @@ class _completeProfile_1State extends State<completeProfile_1> {
     var vheight = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            mySpacer(30.0, vwidth),
-            const Text(
-              "Update Your Account",
-              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
-            ),
-            mySpacer(5.0, vwidth),
-            const Text(
-              "Complete Your Profile",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            mySpacer(30.0, vwidth),
-            Container(
-              height: 520,
-              width: vwidth - 50,
-              padding: const EdgeInsets.all(30),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                color: Colors.white,
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              mySpacer(20.0, vwidth),
+              const Text(
+                "Update Your Account",
+                style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
               ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  myTextfield(
-                    titel: 'NAME',
-                    hint: 'name',
-                    textcontroler: usernameCtrl,
-                  ),
-                  myTextfield(
-                    titel: 'Web or Portfolio',
-                    hint: 'web',
-                    textcontroler: webCtrl,
-                  ),
-                  myTextfield(
-                    titel: 'address',
-                    hint: 'address',
-                    textcontroler: addressCtrl,
-                  ),
-                  myTextfield(
-                    titel: 'MOBILE NUMBER',
-                    hint: 'mobile number',
-                    textcontroler: phonenoCtrl,
-                  ),
-                  myTextfield(
-                    titel: 'Job',
-                    hint: 'Job',
-                    textcontroler: jobCtrl,
-                  ),
-                ],
+              mySpacer(5.0, vwidth),
+              const Text(
+                "Complete Your Profile",
+                style: TextStyle(fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              mySpacer(10.0, vwidth),
+              Container(
+                height: 540,
+                width: vwidth - 50,
+                padding: const EdgeInsets.all(30),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Colors.white,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    myTextfield(
+                      titel: 'NAME',
+                      hint: 'name',
+                      textcontroler: usernameCtrl,
+                    ),
+                    myTextfield(
+                      titel: 'Web or Portfolio',
+                      hint: 'web',
+                      textcontroler: webCtrl,
+                    ),
+                    myTextfield(
+                      titel: 'address',
+                      hint: 'address',
+                      textcontroler: addressCtrl,
+                    ),
+                    myTextfield(
+                      titel: 'MOBILE NUMBER',
+                      hint: 'mobile number',
+                      textcontroler: phonenoCtrl,
+                    ),
+                    myTextfield(
+                      titel: 'Job',
+                      hint: 'Job',
+                      textcontroler: jobCtrl,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,

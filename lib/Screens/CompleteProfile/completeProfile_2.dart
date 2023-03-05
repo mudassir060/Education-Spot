21752ba2d2ff866e954e16eae52509bd204d3670
mudassir060 <mudassirmukhtar4@gmail.com>
2023-03-05 06:37:42 +0500

    add scrol

--- lib/Screens/CompleteProfile/completeProfile_2.dart
+++ lib/Screens/CompleteProfile/completeProfile_2.dart
@@ -28,91 +28,93 @@ class _completeProfile_2State extends State<completeProfile_2> {
     var vheight = MediaQuery.of(context).size.height;
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            mySpacer(30.0, vwidth),
-            const Text(
-              "Update Your Skills",
-              style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
-            ),
-            mySpacer(30.0, vwidth),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  myTextfield(
-                    titel: 'Skill',
-                    hint: 'Skill',
-                    textcontroler: skillCtrl,
-                  ),
-                  Slider(
-                    value: rating,
-                    onChanged: (newrating) {
-                      setState(() {
-                        rating = newrating;
-                      });
-                    },
-                    label: "$rating",
-                    divisions: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      ElevatedButton(
-                          onPressed: () {
-                            skillCtrl.clear();
-                            setState(() {
-                              rating = 0;
-                            });
-                          },
-                          child: Text("Clear")),
-                      ElevatedButton(
-                          onPressed: () {
-                            widget.userData["Skills"].add(
-                                {"name": skillCtrl.text, "rating": rating});
-                            print("++++${widget.userData["Skills"]}");
-                            skillCtrl.clear();
-                            setState(() {
-                              rating = 0;
-                            });
-                          },
-                          child: Text("Add Skill")),
-                    ],
-                  ),
-                  widget.userData["Skills"] != null
-                      ? ListView.builder(
-                          shrinkWrap: true,
-                          physics: const NeverScrollableScrollPhysics(),
-                          itemCount: widget.userData["Skills"].length,
-                          itemBuilder: (BuildContext context, int index) {
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                      "${widget.userData["Skills"][index]["name"]}"),
-                                  SizedBox(
-                                    width: 100,
-                                    child: LinearProgressIndicator(
-                                      value: widget.userData["Skills"][index]
-                                          ["rating"],
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        )
-                      : Text("No Skill"),
-                ],
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              mySpacer(30.0, vwidth),
+              const Text(
+                "Update Your Skills",
+                style: TextStyle(fontSize: 34, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              mySpacer(30.0, vwidth),
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    myTextfield(
+                      titel: 'Skill',
+                      hint: 'Skill',
+                      textcontroler: skillCtrl,
+                    ),
+                    Slider(
+                      value: rating,
+                      onChanged: (newrating) {
+                        setState(() {
+                          rating = newrating;
+                        });
+                      },
+                      label: "$rating",
+                      divisions: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        ElevatedButton(
+                            onPressed: () {
+                              skillCtrl.clear();
+                              setState(() {
+                                rating = 0;
+                              });
+                            },
+                            child: Text("Clear")),
+                        ElevatedButton(
+                            onPressed: () {
+                              widget.userData["Skills"].add(
+                                  {"name": skillCtrl.text, "rating": rating});
+                              print("++++${widget.userData["Skills"]}");
+                              skillCtrl.clear();
+                              setState(() {
+                                rating = 0;
+                              });
+                            },
+                            child: Text("Add Skill")),
+                      ],
+                    ),
+                    widget.userData["Skills"] != null
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemCount: widget.userData["Skills"].length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                        "${widget.userData["Skills"][index]["name"]}"),
+                                    SizedBox(
+                                      width: 100,
+                                      child: LinearProgressIndicator(
+                                        value: widget.userData["Skills"][index]
+                                            ["rating"],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          )
+                        : Text("No Skill"),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         bottomNavigationBar: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,

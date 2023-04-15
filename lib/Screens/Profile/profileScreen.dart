6f6cea997e46a4887bcb2d2ce380f880ed01a1ce
mudassir060 <mudassirmukhtar4@gmail.com>
2023-04-15 15:10:48 +0500

    profile update

--- lib/Screens/Profile/profileScreen.dart
+++ lib/Screens/Profile/profileScreen.dart
@@ -124,25 +124,32 @@ class _profileScreenState extends State<profileScreen> {
                   ),
                   Stack(
                     children: [
-                      CircleAvatar(
-                        radius: 55,
-                        child: widget.UserData["profile"] == null
-                            ? CircleAvatar(
-                                radius: 55,
-                                backgroundImage: AssetImage(Profile),
-                                child: imageLooding == true
-                                    ? CircularProgressIndicator(
-                                        value: progressshow / 100,
-                                        backgroundColor: Colors.purple,
-                                        semanticsValue: progressshow.toString(),
-                                        semanticsLabel: progressshow.toString(),
-                                      )
-                                    : Container(),
-                              )
-                            : CircleAvatar(
-                                radius: 55,
-                                backgroundImage:
-                                    NetworkImage(widget.UserData["profile"])),
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            radius: 55,
+                            child: widget.UserData["profile"] == null
+                                ? CircleAvatar(
+                                    radius: 55,
+                                    backgroundImage: AssetImage(Profile),
+                                  )
+                                : CircleAvatar(
+                                    radius: 55,
+                                    backgroundImage: NetworkImage(
+                                        widget.UserData["profile"])),
+                          ),
+                          imageLooding == true
+                              ? Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: CircularProgressIndicator(
+                                    value: progressshow / 100,
+                                    backgroundColor: Colors.purple,
+                                    semanticsValue: progressshow.toString(),
+                                    semanticsLabel: progressshow.toString(),
+                                  ),
+                                )
+                              : Container(),
+                        ],
                       ),
                       Positioned(
                           bottom: 3,
